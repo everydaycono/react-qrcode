2e3ed5bef1b03089729d9d119f5cab2c7a668dc3
cono <everydaycono@gmail.com>
2022-08-26 22:49:28 +0900

    feat : QR Generator (todo : Download SVG, Style)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { DatePicker, Input, Menu, Select } from "antd"
+import { Header } from 'antd/lib/layout/layout'
+import Navbar from './components/Navbar'
+import { Option } from 'antd/lib/mentions';
+import React, { useState } from 'react';
+
+import QRCode from "react-qr-code";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [isSelf, setIsSelf] = useState(false);
+  const [value, setValue] = useState("");
+  const [size, setSize] = useState("200");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  }
+  const handleSize = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSize(e.currentTarget.value);
+  }
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+
+      <main  >
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }} >
+          <div >
+            <h1>QR CODE Generator</h1>
+            <p>QR codes allow smartphone users to acces your websites wimply and quickqly</p>
+
+            <div>
+              <input onChange={handleChange} type="text" />
+              {isSelf && <Input placeholder="Basic usage" />}
+            </div>
+            <select onChange={handleSize}>
+              <option value="200">200</option>
+              <option value="300">300</option>
+              <option value="400">400</option>
+              <option value="500">500</option>
+            </select>
+            <button>Generate a Qrcode</button>
+          </div>
+          <img style={{ width: "300px" }} src="https://images.prismic.io/bizzonwebsite/db7a23ba-4a87-4aa5-a9dd-364f02c7fe22_Digital+Ordering+Tips-01.png?auto=compress,format" alt="qr-code" />
+        </div>
+
+        {value !== "" &&
+          <QRCode size={Number(size)} value={value} />
+        }
+      </main>
     </div>
   )
 }
